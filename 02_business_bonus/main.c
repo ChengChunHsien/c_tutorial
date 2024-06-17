09d1df088d44459e9e49dafcd86bca2f96ca4c00
Charls <scjcharls@outlook.com>
2024-06-17 22:40:12 +0800

    complete the problem no.2

--- 02_business_bonus/main.c
+++ 02_business_bonus/main.c
@@ -40,6 +40,51 @@ the example is
 52634.000000
 */
 
+// #include "stdio.h"
+// #include "stdlib.h"
+
+// int main()
+// {
+//     double profit = 0.0;
+//     double total_bonus = 0.0;
+
+//     // input the profit
+//     printf("input the profit\n");
+//     scanf("%lf", &profit);
+//     printf("the profit is : %lf\n", profit);
+
+//     // caculate the bonus based on profit
+//     if (profit <= 100000)
+//     {
+//         total_bonus = profit * 0.1;
+//     }
+//     else if (profit <= 200000)
+//     {
+//         total_bonus = (100000 * 0.1) + ((profit - 100000) * 0.075);
+//     }
+//     else if (profit <= 400000)
+//     {
+//         total_bonus = (100000 * 0.1) + (100000 * 0.075) + ((profit - 200000) * 0.05);
+//     }
+//     else if (profit <= 600000)
+//     {
+//         total_bonus = (100000 * 0.1) + (100000 * 0.075) + (200000 * 0.05) + ((profit - 400000) * 0.03);
+//     }
+//     else if (profit <= 1000000)
+//     {
+//         total_bonus =
+//             (100000 * 0.1) + (100000 * 0.075) + (200000 * 0.05) + (400000 * 0.03) + ((profit - 600000) * 0.015);
+//     }
+//     else if (profit > 1000000)
+//     {
+//         total_bonus = (100000 * 0.1) + (100000 * 0.075) + (200000 * 0.05) + (400000 * 0.03) + (600000 * 0.015) + ((profit - 1000000) * 0.01);
+//     }
+
+//     printf("the total bonus is : %.2f\n", total_bonus);
+
+//     return 0;
+// }
+
 #include "stdio.h"
 #include "stdlib.h"
 
@@ -51,37 +96,41 @@ int main()
     // input the profit
     printf("input the profit\n");
     scanf("%lf", &profit);
+    printf("----------------------------------------\n");
     printf("the profit is : %lf\n", profit);
 
-    // caculate the bonus based on profit
+    // calculate the bonus based on profit
+    if (profit > 1000000)
+    {
+        total_bonus += ((profit - 1000000) * 0.01);
+        profit = 1000000;
+    }
+    if (profit > 600000)
+    {
+        total_bonus += ((profit - 600000) * 0.015);
+        profit = 600000;
+    }
+    if (profit > 400000)
+    {
+        total_bonus += ((profit - 400000) * 0.03);
+        profit = 400000;
+    }
+    if (profit > 200000)
+    {
+        total_bonus += ((profit - 200000) * 0.05);
+        profit = 200000;
+    }
+    if (profit > 100000)
+    {
+        total_bonus += ((profit - 100000) * 0.075);
+        profit = 100000;
+    }
     if (profit <= 100000)
     {
-        total_bonus = profit * 0.1;
-    }
-    else if (profit <= 200000)
-    {
-        total_bonus = (100000 * 0.1) + ((profit - 100000) * 0.075);
-    }
-    else if (profit <= 400000)
-    {
-        total_bonus = (100000 * 0.1) + (100000 * 0.075) + ((profit - 200000) * 0.05);
-    }
-    else if (profit <= 600000)
-    {
-        total_bonus = (100000 * 0.1) + (100000 * 0.075) + (200000 * 0.05) + ((profit - 400000) * 0.03);
-    }
-    else if (profit <= 1000000)
-    {
-        total_bonus =
-            (100000 * 0.1) + (100000 * 0.075) + (200000 * 0.05) + (400000 * 0.03) + ((profit - 600000) * 0.015);
-    }
-    else if (profit > 1000000)
-    {
-        total_bonus = (100000 * 0.1) + (100000 * 0.075) + (200000 * 0.05) + (400000 * 0.03) + (600000 * 0.015) +
-                      ((profit - 1000000) * 0.01);
+        total_bonus += (profit * 0.1);
     }
 
     printf("the total bonus is : %.2f\n", total_bonus);
 
-    return -1;
+    return 0;
 }
